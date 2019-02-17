47507c6da086e830f142afb0967df2f5cdad4309
Kelly Selden <kellyselden@gmail.com>
2019-02-17 13:50:32 +0000

    rule upgrades

--- tests/lib/rules/no-branch-in-dependencies.js
+++ tests/lib/rules/no-branch-in-dependencies.js
@@ -2,8 +2,7 @@
 
 const { RuleTester } = require('eslint');
 const rule = require('../../../lib/rules/no-branch-in-dependencies');
-
-const { processors: { '.json': processor } } = require('../../../lib');
+const processor = require('../../../lib/processors/json');
 
 // RuleTester doesn't allow preprocessors
 function preprocess(item) {
@@ -11,19 +10,66 @@ function preprocess(item) {
   return item;
 }
 
-new RuleTester().run('no-branch-in-dependencies', rule, {
-  valid: [
+function fix(tests) {
+  for (let type of Object.keys(tests)) {
+    tests[type] = tests[type].map(preprocess);
+  }
+  return tests;
+}
 
-    // give me some code that won't trigger a warning
+new RuleTester().run('no-branch-in-dependencies', rule, fix({
+  valid: [
+    {
+      code: '{ "dependencies": { "lodash": "1.2.3" } }',
+      filename: 'package.json'
+    },
+    {
+      code: '{ "devDependencies": { "lodash": "1.2.3" } }',
+      filename: 'package.json'
+    },
+    {
+      code: '{ "optionalDependencies": { "lodash": "1.2.3" } }',
+      filename: 'package.json'
+    },
+    {
+      code: '{ "foo": { "lodash": "lodash/lodash" } }',
+      filename: 'package.json'
+    },
+    {
+      code: '{ "dependencies": { "lodash": "lodash/lodash" } }',
+      filename: 'package.json',
+      options: [{ keys: ['foo'] }]
+    },
+    {
+      code: '{ "dependencies": { "lodash": "lodash/lodash" } }',
+      filename: 'package.json',
+      options: [{ exclude: ['lodash'] }]
+    }
   ],
   invalid: [
-    preprocess({
+    {
+      code: '{ "dependencies": { "lodash": "lodash/lodash" } }',
+      filename: 'package.json',
+      errors: [{
+        message: 'Don\'t use branches.',
+        type: 'Literal'
+      }]
+    },
+    {
       code: '{ "devDependencies": { "lodash": "lodash/lodash" } }',
       filename: 'package.json',
       errors: [{
         message: 'Don\'t use branches.',
         type: 'Literal'
       }]
-    })
+    },
+    {
+      code: '{ "optionalDependencies": { "lodash": "lodash/lodash" } }',
+      filename: 'package.json',
+      errors: [{
+        message: 'Don\'t use branches.',
+        type: 'Literal'
+      }]
+    }
   ]
-});
+}));
