d23fd03772e7bd2f25fe187b55e918a219d95ac7
Kelly Selden <kellyselden@gmail.com>
2019-07-20 19:36:01 +0100

    fix autofixing verify

--- tests/helpers/preprocess.js
+++ tests/helpers/preprocess.js
@@ -3,14 +3,28 @@
 const processor = require('../../lib/processors/json');
 
 // RuleTester doesn't allow preprocessors
-function preprocess(item) {
+function _preprocess(item) {
   item.code = processor.preprocess(item.code, item.filename)[0];
   return item;
 }
 
-module.exports = function fix(tests) {
+function preprocess(tests) {
   for (let type of Object.keys(tests)) {
-    tests[type] = tests[type].map(preprocess);
+    tests[type] = tests[type].map(_preprocess);
   }
   return tests;
-};
+}
+
+function applyAutofixWorkaround(ruleTester) {
+  let { linter, linter: { verify } } = ruleTester;
+  linter.verify = function(output, ...args) {
+    if (!output.startsWith(processor._prefix)) {
+      output = _preprocess({ code: output }).code;
+    }
+    return verify.call(this, output, ...args);
+  };
+  return ruleTester;
+}
+
+module.exports = preprocess;
+module.exports.applyAutofixWorkaround = applyAutofixWorkaround;
