c578533585b900edc2ae217e3a7ff622a46c342c
Caleb Cox <canac@users.noreply.github.com>
2021-05-12 17:08:33 -0500

    Preserve trailing whitespace when sorting

--- tests/lib/rules/sort-package-json.js
+++ tests/lib/rules/sort-package-json.js
@@ -39,6 +39,22 @@ preprocess.applyAutofixWorkaround(new RuleTester()).run('sort-package-json', rul
 }
 `
     },
+    // preserves no trailing whitespace
+    {
+      code: `{
+  "version": "1.0.0",
+  "name": "foo"
+}`,
+      filename: 'package.json',
+      errors: [{
+        message: 'package.json is not sorted correctly.',
+        type: 'ObjectExpression'
+      }],
+      output: `{
+  "name": "foo",
+  "version": "1.0.0"
+}`
+    },
     // preserves existing indentation
     {
       code: `{
