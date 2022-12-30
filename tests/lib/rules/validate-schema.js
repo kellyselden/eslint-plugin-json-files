6dfdf5be4d602b57ba0e3a1ee1f8aff876f58558
Kelly Selden <kellyselden@gmail.com>
2022-12-29 19:10:44 -0800

    feat: use better-ajv-errors for better json-schema formatting

--- tests/lib/rules/validate-schema.js
+++ tests/lib/rules/validate-schema.js
@@ -3,6 +3,11 @@
 const { RuleTester } = require('eslint');
 const rule = require('../../../lib/rules/validate-schema');
 const preprocess = require('../../helpers/preprocess');
+const stripAnsi = require('strip-ansi');
+
+function color(s) {
+  return process.stdout.isTTY ? s : stripAnsi(s);
+}
 
 new RuleTester().run('validate-schema', rule, preprocess({
   valid: [
@@ -32,7 +37,7 @@ new RuleTester().run('validate-schema', rule, preprocess({
         })
       }],
       errors: [{
-        message: '#/not must NOT be valid',
+        message: color('[31m[1mNOT[22m[39m[31m must NOT be valid[39m\n\n[0m[31m[1m>[22m[39m[90m 1 |[39m {[32m"foo"[39m[33m:[39m[32m"bar"[39m}[0m\n[0m [90m   |[39m [31m[1m^[22m[39m[31m[1m^[22m[39m[31m[1m^[22m[39m[31m[1m^[22m[39m[31m[1m^[22m[39m[31m[1m^[22m[39m[31m[1m^[22m[39m[31m[1m^[22m[39m[31m[1m^[22m[39m[31m[1m^[22m[39m[31m[1m^[22m[39m[31m[1m^[22m[39m[31m[1m^[22m[39m [31m[1m👈🏽  [95mnot[31m must NOT be valid[22m[39m[0m'),
         type: 'ObjectExpression'
       }]
     }
