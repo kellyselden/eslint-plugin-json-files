aa34fe7496f6bd75f1e4759d5fa994a22744592e
Kelly Selden <602423+kellyselden@users.noreply.github.com>
2025-09-01 16:06:49 -0400

    don't use real config in tests

--- tests/lib/processors/json.js
+++ tests/lib/processors/json.js
@@ -6,62 +6,61 @@ const { ESLint } = require('eslint');
 const jsonFiles = require('../../../lib');
 
 describe(function() {
-  let cli;
-
-  before(function() {
-    cli = new ESLint({
+  async function _test({ rules, fix, text }) {
+    let cli = new ESLint({
       ignore: false,
+      overrideConfigFile: true,
       overrideConfig: {
         files: ['package.json'],
-        rules: {
-          'quote-props': [2, 'as-needed'],
-          'json-files/require-license': [2],
-        },
+        rules,
         plugins: {
           'json-files': jsonFiles,
         },
+        processor: jsonFiles.processors.json,
       },
+      fix,
     });
-  });
-
-  it('should run plugin on .json files', async function() {
-    let text = `{
-  license: "UNLICENSED"
-}
-`;
 
     let results = await cli.lintText(text, { filePath: 'package.json' });
 
     expect(results.length).to.equal(1);
+
+    return results;
+  }
+
+  it('should run plugin on .json files', async function() {
+    let text = `{
+  "license": "UNLICENSED"
+}
+`;
+
+    let results = await _test({
+      rules: {
+        'json-files/require-license': [2],
+      },
+      text,
+    });
+
     expect(results[0].messages.length).to.equal(1);
     expect(results[0].messages[0].message).to.equal('Missing license.');
-    expect(results[0].messages[0].line).to.equal(1);
+    expect(results[0].messages[0].line).to.equal(2);
   });
 
   it('should fix issues in .json files', async function() {
-    let fixerCli = new ESLint({
-      ignore: false,
-      overrideConfig: {
-        files: ['package.json'],
-        rules: {
-          'json-files/sort-package-json': [2],
-        },
-        plugins: {
-          'json-files': jsonFiles,
-        },
-      },
-      fix: true,
-    });
-
     let text = `{
   "version": "1.0.0",
   "name": "foo"
 }
 `;
 
-    let results = await fixerCli.lintText(text, { filePath: 'package.json' });
+    let results = await _test({
+      rules: {
+        'json-files/sort-package-json': [2],
+      },
+      fix: true,
+      text,
+    });
 
-    expect(results.length).to.equal(1);
     expect(results[0].output).to.equal(`{
   "name": "foo",
   "version": "1.0.0"
@@ -71,25 +70,35 @@ describe(function() {
 
   it('should ignore js rules on .json files', async function() {
     let text = `{
-  "license": "MIT"
+  "foo": "",
+  "bar": ""
 }
 `;
 
-    let results = await cli.lintText(text, { filePath: 'package.json' });
+    let results = await _test({
+      rules: {
+        'sort-keys': [2],
+      },
+      text,
+    });
 
-    expect(results.length).to.equal(1);
     expect(results[0].messages.length).to.equal(0);
   });
 
   it.skip('should run js rules on .json files', async function() {
     let text = `{
-  "license": "MIT"
+  "foo": "",
+  "bar": ""
 }
 `;
 
-    let results = await cli.lintText(text, { filePath: 'package.json' });
+    let results = await _test({
+      rules: {
+        'sort-keys': [2],
+      },
+      text,
+    });
 
-    expect(results.length).to.equal(1);
     expect(results[0].messages.length).to.equal(1);
     expect(results[0].messages[0].message).to.equal('Unnecessarily quoted property \'license\' found.');
     expect(results[0].messages[0].line).to.equal(2);
